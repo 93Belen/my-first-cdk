cb372259470be1b485e710e9f1c9bc42df264de8
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-09-28 10:19:38 -0500

    to sctric equeal misspelling

--- test/my-first-cdk-app.test.ts
+++ test/my-first-cdk-app.test.ts
@@ -3,11 +3,11 @@ import { handler } from "../src";
 describe("My First CDK app Test", () => {
     it("returns name and tesks", async () => {
         const output = await handler({ name: "Faruk"});
-        expect(output).toStricEqual("Good Job Faruk!")
+        expect(output).toStrictEqual("Good Job Faruk!")
     })
     it("returns no name and tesks", async () => {
         const output = await handler({ name: ""});
-        expect(output).toStricEqual("Good Job")
+        expect(output).toStrictEqual("Good Job")
     })
 
 })
